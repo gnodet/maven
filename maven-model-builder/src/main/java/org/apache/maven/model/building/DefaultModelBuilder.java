0d8687559a7386dfb6f13a484bcfc003a11d3d0c
Guillaume Nodet <gnodet@gmail.com>
2019-06-28 10:23:08 +0200

    Avoid full interpolation if not needed

--- maven-model-builder/src/main/java/org/apache/maven/model/building/DefaultModelBuilder.java
+++ maven-model-builder/src/main/java/org/apache/maven/model/building/DefaultModelBuilder.java
@@ -403,9 +403,12 @@ public class DefaultModelBuilder
         if ( resultModel.getParent() != null )
         {
             final ModelData parentData = lineage.get( 1 );
-            final Model interpolatedParent = interpolateModel( parentData.getModel(), request, problems );
-            // parentData.setModel( interpolatedParent );
-            parentData.setVersion( interpolatedParent.getVersion() );
+            if ( parentData.getVersion() == null || parentData.getVersion().contains( "${" ) )
+            {
+                final Model interpolatedParent = interpolateModel( parentData.getModel(), request, problems );
+                // parentData.setModel( interpolatedParent );
+                parentData.setVersion( interpolatedParent.getVersion() );
+            }
         }
 
         // url normalization
