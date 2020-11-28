acd2136f8b264b3a69154a08862ee4c6273010fc
Guillaume Nodet <gnodet@gmail.com>
2020-11-28 22:27:49 +0100

    Do not use a "persistent" cache which is the root cause MNG-6530
    The root of the problem is MNG-5312 which is a performance issue because a ModelCache was not always involved during a model build.  This lead to MNG-6030 (memory consumption issue because of the usage of multiple caches). The fix for MNG-6030 lead to MNG-6311 (problem with changes not being picked up in M2E).
    The root problem is that the fix for MNG-5312 was wrong.  The caches and its data should be managed at the RepositorySystemSession level, especially as the data is related to a given session parameters (maven global settings).
    This commit brings back the cache at the session level, and a default cache is now set on the session by the MavenCli.  The cache is used later by DefaultArtifactDescriptorReader and DefaultProjectBuilder to build a DefaultModelCache.
    
    * use the cache on the MavenExecutionRequest
    * get rid of the hack for  MNG-6530.
    * MavenCli configures a cache for the whole execution request
    * merge ReactorModelCache into DefaultModelCache
    
    # Conflicts:
    #	maven-core/src/main/java/org/apache/maven/project/DefaultProjectBuilder.java
    #	maven-core/src/main/java/org/apache/maven/project/ReactorModelCache.java

--- maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -82,6 +82,7 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 import org.codehaus.plexus.logging.LoggerManager;
 import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.eclipse.aether.DefaultRepositoryCache;
 import org.eclipse.aether.transfer.TransferListener;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
@@ -973,6 +974,11 @@ public class MavenCli
         throws MavenExecutionRequestPopulationException
     {
         MavenExecutionRequest request = executionRequestPopulator.populateDefaults( cliRequest.request );
+
+        if ( cliRequest.request.getRepositoryCache() == null )
+        {
+            cliRequest.request.setRepositoryCache( new DefaultRepositoryCache() );
+        }
 
         eventSpyDispatcher.onEvent( request );
 
