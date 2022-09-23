cf97ddb15696145b200a6fb34759edbca53a4a28
Guillaume Nodet <gnodet@gmail.com>
2022-09-23 16:54:13 +0200

    Remove Metadata from the v4 api

--- maven-core/src/main/java/org/apache/maven/internal/impl/DefaultArtifactManager.java
+++ maven-core/src/main/java/org/apache/maven/internal/impl/DefaultArtifactManager.java
@@ -19,18 +19,13 @@ package org.apache.maven.internal.impl;
  * under the License.
  */
 
-import org.apache.maven.api.annotations.Nonnull;
-
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.apache.maven.api.Artifact;
-import org.apache.maven.api.Metadata;
+import org.apache.maven.api.annotations.Nonnull;
 import org.apache.maven.api.services.ArtifactManager;
 import org.apache.maven.project.MavenProject;
 
@@ -40,7 +35,6 @@ public class DefaultArtifactManager implements ArtifactManager
     @Nonnull
     private final DefaultSession session;
     private final Map<Artifact, Path> paths = new ConcurrentHashMap<>();
-    private final Map<Artifact, Collection<Metadata>> metadatas = new ConcurrentHashMap<>();
 
     public DefaultArtifactManager( @Nonnull DefaultSession session )
     {
@@ -89,20 +83,6 @@ public class DefaultArtifactManager implements ArtifactManager
         {
             paths.put( artifact, path );
         }
-    }
-
-    @Nonnull
-    @Override
-    public Collection<Metadata> getAttachedMetadatas( @Nonnull Artifact artifact )
-    {
-        Collection<Metadata> m = metadatas.get( artifact );
-        return m != null ? Collections.unmodifiableCollection( m ) : Collections.emptyList();
-    }
-
-    @Override
-    public void attachMetadata( @Nonnull Artifact artifact, @Nonnull Metadata metadata )
-    {
-        metadatas.computeIfAbsent( artifact, a -> new CopyOnWriteArrayList<>() ).add( metadata );
     }
 
     private String id( org.apache.maven.artifact.Artifact artifact )
